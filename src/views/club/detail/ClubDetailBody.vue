0b92ccf8e5cee781718f52a7412a31d2eec6353d
hwanse <khsb2012@gmail.com>
2021-07-06 00:23:39 +0900

    detail view body 활용

---
+++ src/views/club/detail/ClubDetailBody.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-tabs v-model="tab" grow background-color="primary" dark> 
+            <v-tab v-for="menu in menus"
+                    :key="menu.id">
+                {{ menu.name }}
+            </v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="tab">
+        
+            <v-tab-item v-for="menu in menus" :key="menu.id">
+                
+            </v-tab-item>
+        
+        </v-tabs-items>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tab: null,
+            menus: [
+                {id: 1, name: '메뉴1'},
+                {id: 2, name: '메뉴2'},
+                {id: 3, name: '메뉴2'},
+                {id: 4, name: '메뉴2'},
+            ]
+        }
+    },
+}
+</script>
---
+++ src/views/club/detail/ClubDetailView.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container class="pa-0">
+        <club-detail-body></club-detail-body>
+    </v-container>
+</template>
+
+<script>
+import ClubDetailBody from './ClubDetailBody.vue'
+
+export default {
+    components: {
+        'club-detail-body': ClubDetailBody
+    }    
+}
+</script>
